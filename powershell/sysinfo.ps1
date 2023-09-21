25371ff87763dfa10c10d21daed252a51b9f294c
Sean McDerment <71660010+SeanMcDerment@users.noreply.github.com>
2023-09-21 14:17:03 -0400

    Add files via upload

---
+++ powershell/sysinfo.ps1
@@ -0,0 +1,16 @@
+﻿function getIP{
+    (get-netipaddress).ipv4address | Select-String "192*"
+}
+$User = $env:Username
+$IP = getIP
+$ver = $PSVersionTable.PSVersion.Major
+$DATE = getDate
+$HOSTname = $env:COMPUTERNAME
+
+$SMTPServer = "smtp.gmail.com"
+$SenderEmail = "seanmcderment@gmail.com"
+$RecipientEmail = "seanmcderment@gmail.com"
+
+$BODY = "This machine's IP is $IP. User is $User. Hostname is $HOSTname. PowerShell $ver. Today's Date is $DATE."
+
+Send-MailMessage -To "seanmcderment@gmail.com" -From "seanmcderment@gmail.com" -Subject "IT3038C Windows SysInfo" -Body $BODY -SmtpServer smtp.gmail.com -port 587 -UseSSL -Credential (Get-Credential) 
